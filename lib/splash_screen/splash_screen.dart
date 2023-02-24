0a34a218cca79a3e81b0486fa7f41307f95b220f
Regene <regene@pop-os.localdomain>
2023-02-24 09:30:21 +0700

    init project

---
+++ lib/splash_screen/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(100),
+            alignment: Alignment.center,
+            height: 120.0,
+            width: 120.0,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/download.jpeg')),
+              shape: BoxShape.circle,
+            ),
+          ),
+          Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'CamTech',
+                style: TextStyle(fontSize: 20),
+              )),
+        ],
+      )),
+    );
+  }
+}
